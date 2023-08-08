2deb36dd1b614425e24be635d4b5d18a8e09d182
Maria Antonia Galdino <107472413+MariaAntoniaGaldino@users.noreply.github.com>
2023-08-07 21:36:10 -0300

    Add files via upload

---
+++ Sistema de vendas/Pessoa.java
@@ -0,0 +1,17 @@
+Public class Cliente {
+Public String nome; 
+Public String endereço;     
+}
+
+public void cadastrar() {
+    Scanner cad = New Scanner (System.in) :
+    System.out.printlin("Nome: ")
+    this.nome = cad.nextLine () :
+    System.out.printlin("Endereço: ")
+    this.endereco = cad.nextLine () :
+
+
+}
+
+Vendedor
+Gerente 
